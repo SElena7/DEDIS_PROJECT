986d5517015f93f50734e40674585266fa08da1e
sstalevska <sara.stalevska@gmail.com>
2024-12-02 01:34:48 +0100

    Add pages names in nav.

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -6,16 +6,22 @@ function Nav() {
     <div className="nav-container">
       <ul className="nav-list">
         <li className="nav-item">
-          <a href="#" className="nav-link">Home</a>
+          <a href="#" className="nav-link">About</a>
         </li>
         <li className="nav-item">
-          <a href="#about" className="nav-link">About</a>
+          <a href="#about" className="nav-link">Newsfeed</a>
         </li>
         <li className="nav-item">
-          <a href="#services" className="nav-link">Services</a>
+          <a href="#services" className="nav-link">Enrollment</a>
         </li>
         <li className="nav-item">
-          <a href="#contact" className="nav-link">Contact</a>
+          <a href="#contact" className="nav-link">Gallery</a>
+        </li>
+        <li className="nav-item">
+          <a href="#contact" className="nav-link">Schedule</a>
+        </li>
+        <li className="nav-item">
+          <a href="#contact" className="nav-link">Donations</a>
         </li>
       </ul>
     </div>
